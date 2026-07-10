05903a20989c86291f409233d3de6524ef1a5c6e
Ubuntu <ubuntu@localhost.localdomain>
2026-07-10 14:05:39 +0800

    fix: correct SQL parameter count in deposit_end_storage (5 placeholders, 4 params)

--- routes/user.py
+++ routes/user.py
@@ -1117,7 +1117,7 @@ def deposit_end_storage():
                                (refund_amount, refund_amount, order['user_phone'], _openid))
             else:
                 cursor.execute("UPDATE user_balances SET balance = balance + %s, total_deposited = total_deposited + %s, openid = %s WHERE phone = %s AND (openid = %s OR openid IS NULL OR openid = '')",
-                               (refund_amount, refund_amount, order['user_phone'], ''))
+                               (refund_amount, refund_amount, '', order['user_phone'], ''))
         else:
             # 查询wechat_name（忽略游标异常，避免阻止订单结束）
             _wechat_name2 = ''
